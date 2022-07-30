b4029ad12a5e2a7f09e781332c2f02776531a378
Chase W <cwwalters@fastmail.com>
2022-07-30 15:14:00 -0600

    fixed issue with webpack trying to parse sample-objects.json (#34)

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
-        exclude: /node_modules/,
+        exclude: /node_modules|sample-objects.json/,
         use: {
           loader: 'babel-loader',
           options: {
