1f372bfb59fe39f771c7f65657ade8126af1d649
Samson Lam <104582906+sflam2013@users.noreply.github.com>
2022-07-31 00:53:21 +0800

    bot server upudate1' (#14)
    * bot server upudate1'
    
    * bot update2

--- client/index.js
+++ client/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
+
 
 render(
   <Provider store={store}>
