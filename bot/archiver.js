6a84070a3768cee3798dbefe187aeb3b0c708db4
Chase Walters <chasew@fastmail.com>
2022-07-31 20:06:37 -0600

    Unit testing for archiver, integration testing for API

--- bot/archiver.js
+++ bot/archiver.js
@@ -5,7 +5,6 @@ const archiver = {};
 archiver.send = async (message) => {
   try {
     const result = await axios.put( 'http://localhost:3000/api/resource', message );
-    console.log(`Archiver: Successfully sent message to API ${result.status} ${result.stausText}`)
     return result;
   } catch (err) {
     console.error(`Archiver: Failed to send message to API: `, err);
